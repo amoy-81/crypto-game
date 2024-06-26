9bea3e6d30a3e269d6bb06f989b64d20d21d08bc
alim1381 <ali.moaydi2020@gmail.com>
2024-06-26 16:13:23 +0330

    client configs for vercel

--- server/src/modules/friends/friends.router.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import friendsController from "./friends.controller";
-import authGuard from "../../common/guards/auth.guard";
-const FriendsRouter = express.Router();
-
-FriendsRouter.get("/", authGuard, friendsController.getUserFriends);
-FriendsRouter.get("/il", authGuard, friendsController.generateIl);
-
-export default FriendsRouter;
